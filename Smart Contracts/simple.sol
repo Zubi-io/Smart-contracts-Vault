e7b05821878a269bd8033c4eac66be2042e97b9d
Allen Joseph <allenxavier45@gmail.com>
2020-09-04 12:27:13 +0530

    Add files via upload

---
+++ Smart Contracts/simple.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.21;
+contract Hello {
+    string public message;
+    function Hello(string initialMessage) public {
+        message = initialMessage;
+    }
+    //Add a simple setMessage function here to change the message variable.
+}
