f646fc72fb2609a175509dc9d37549675ed610f2
Allen Joseph <allenxavier45@gmail.com>
2020-09-09 12:21:34 +0530

    Add files via upload

---
+++ Smart Contracts/storage.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.4.0 <0.7.0;
+
+contract SimpleStorage {
+    uint storedData;
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    // add a function get to return the storedData
+}
