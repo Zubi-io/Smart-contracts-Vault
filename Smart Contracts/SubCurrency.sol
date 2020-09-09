355884433d15b80d6a07498086439de5effa9c50
Allen Joseph <allenxavier45@gmail.com>
2020-09-09 12:27:50 +0530

    Add files via upload

---
+++ Smart Contracts/SubCurrency.sol
@@ -0,0 +1,32 @@
+pragma solidity >=0.5.0 <0.7.0;
+
+contract Coin {
+    // The keyword "public" makes variables
+    // accessible from other contracts
+    address public minter;
+    mapping (address => uint) public balances;
+
+    // Events allow clients to react to specific
+    // contract changes you declare
+    event Sent(address from, address to, uint amount);
+
+    // Constructor code is only run when the contract
+    // is created
+    constructor() public {
+        minter = msg.sender;
+    }
+
+    // Add a function mint which:
+    // Sends an amount of newly created coins to an address
+    // Can only be called by the contract creator
+    function mint(address receiver, uint amount) public {
+
+    }
+
+    // Add a function send which:
+    // Sends an amount of existing coins
+    // from any caller to an address
+    function send(address receiver, uint amount) public {
+
+    }
+}
